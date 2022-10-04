1ae60595e38542e7665d8077004094b932d5bc90
Luan <luanoliveira17@hotmail.com>
2022-10-04 20:34:53 -0300

    Adicionando typescript

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/games', (req, res) => {
+  return res.json([])
+})
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([])
+})
+
+app.get('/games/:id/ads', (req, res) => {
+  const gameId = req.params.id
+
+  return res.send(gameId)
+  return res.json([
+    { id: 1, name: 'Anúncio 1' },
+    { id: 2, name: 'Anúncio 2' },
+    { id: 3, name: 'Anúncio 3' },
+    { id: 4, name: 'Anúncio 4' },
+    { id: 5, name: 'Anúncio 5' }
+  ])
+})
+
+app.listen(3333)
